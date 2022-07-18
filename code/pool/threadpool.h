dc21cea1033192361701597b1ac706568c73e572
tae <zhuzhanzzx@163.com>
2022-07-18 18:32:59 +0800

    Add thread pool comments.

--- code/pool/threadpool.h
+++ code/pool/threadpool.h
@@ -16,21 +16,25 @@ class ThreadPool {
 public:
     explicit ThreadPool(size_t threadCount = 8): pool_(std::make_shared<Pool>()) {
             assert(threadCount > 0);
+
+            // create threadCount threads
             for(size_t i = 0; i < threadCount; i++) {
                 std::thread([pool = pool_] {
                     std::unique_lock<std::mutex> locker(pool->mtx);
                     while(true) {
                         if(!pool->tasks.empty()) {
+                            // get a tasks from queue
                             auto task = std::move(pool->tasks.front());
+                            // remove the task
                             pool->tasks.pop();
                             locker.unlock();
-                            task();
+                            task(); // task code
                             locker.lock();
                         } 
-                        else if(pool->isClosed) break;
-                        else pool->cond.wait(locker);
+                        else if(pool->isClosed) break; // delete thread
+                        else pool->cond.wait(locker); // block thread
                     }
-                }).detach();
+                }).detach(); // thread detach
             }
     }
 
@@ -44,7 +48,7 @@ public:
                 std::lock_guard<std::mutex> locker(pool_->mtx);
                 pool_->isClosed = true;
             }
-            pool_->cond.notify_all();
+            pool_->cond.notify_all(); // weak all thread
         }
     }
 
@@ -52,17 +56,18 @@ public:
     void AddTask(F&& task) {
         {
             std::lock_guard<std::mutex> locker(pool_->mtx);
-            pool_->tasks.emplace(std::forward<F>(task));
+            pool_->tasks.emplace(std::forward<F>(task)); // add task to queue
         }
-        pool_->cond.notify_one();
+        pool_->cond.notify_one(); // weak a thread
     }
 
 private:
+    // define pool struct
     struct Pool {
         std::mutex mtx;
         std::condition_variable cond;
         bool isClosed;
-        std::queue<std::function<void()>> tasks;
+        std::queue<std::function<void()>> tasks; // queue(store tasks)
     };
     std::shared_ptr<Pool> pool_;
 };
